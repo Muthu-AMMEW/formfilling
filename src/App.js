72f7fec225e6753461acabcb065778fea6584d87
Muthu-AMMEW <muthu.ammew@gmail.com>
2024-08-07 12:55:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h3 className="container-fluid text-bg-light text-info">Welcome to my world</h3>
+    </>
+  );
+}
+
+export default App;
